9ae9cb8b39432343a8972572309a68e179a59a8b
Daniel Mora <develop@danielmora.cr>
2017-05-14 21:22:22 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] }, exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] }, exclude: /node_modules/ },
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
